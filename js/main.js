b946e8eedd55ed3c78b99516cfa914ae5ff10433
Yuri <iurii_bardych@hotmail.com>
2022-11-28 20:26:40 +0100

    updates 6

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 const filterButtons = document.querySelectorAll(".filter-button");
 for (let i = 0; i < filterButtons.length; i++) {
-	filterButtons[i].onclick = function () {
-		document.querySelectorAll(".filter-button").forEach((button) => {
-			button.classList.remove("active");
-		});
-		filterButtons[i].classList.add("active");
-		myFunction(filterButtons[i].getAttribute("data-filter"));
-	};
+  filterButtons[i].onclick = function () {
+    document.querySelectorAll(".filter-button").forEach((button) => {
+      button.classList.remove("active");
+    });
+    filterButtons[i].classList.add("active");
+    myFunction(filterButtons[i].getAttribute("data-filter"));
+  };
 }
 
 function myFunction(filter) {
-	const PorjectCards = document.querySelectorAll(".card");
-	for (let i = 0; i < PorjectCards.length; i++) {
-		if (
-			filter == PorjectCards[i].getAttribute("data-filter") ||
-			filter == "all"
-		) {
-			PorjectCards[i].style.display = "flex";
-		} else {
-			PorjectCards[i].style.display = "none";
-		}
-	}
+  const PorjectCards = document.querySelectorAll(".project-cards-item");
+  for (let i = 0; i < PorjectCards.length; i++) {
+    if (
+      filter == PorjectCards[i].getAttribute("data-filter") ||
+      filter == "all"
+    ) {
+      PorjectCards[i].style.display = "flex";
+    } else {
+      PorjectCards[i].style.display = "none";
+    }
+  }
 }
